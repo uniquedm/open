5c512b2223f3750a5b8e4da06fb8f9f802e2c172
uniquedm <47504086+uniquedm@users.noreply.github.com>
2019-04-26 12:50:47 +0530

    Add files via upload

--- write.php
+++ write.php
@@ -1,14 +1,17 @@
 <?php
-$myfile = fopen("message.txt", "w") or die("Unable to open file!");
 if(isset($_POST["message"]))
 {
-$txt = $_POST["message"]."\n";
+$myfile = fopen("message.txt", "r");
+$x = fread($myfile, "100000");
+$txt = $x."\n".$_POST["message"]."\n";
+fclose($myfile);
+$myfile = fopen("message.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $txt);
 }
 fclose($myfile);
 
 $myfile = fopen("http://192.168.6.185/message.txt", "r") or die("Unable to open file!");
-echo fread($myfile, "15"); 
+echo fread($myfile, "100000"); 
 fclose($myfile);
 ?>
 
