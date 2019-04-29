084daa706cefc64bd60347d197bb6b2566348828
uniquedm <47504086+uniquedm@users.noreply.github.com>
2019-04-29 12:54:01 +0530

    Add files via upload

--- chat.php
+++ chat.php
@@ -1,19 +1,39 @@
-<HTML>
-<TITLE>X8 Chat</TITLE>
-<BODY>
-	<FORM action="<?php $_PHP_SELF?>" method="post">
-		<input type="text" name="x" >
- 		<input type="submit" value="Send">
-	</FORM>
-</BODY>
 <?php
-	if(isset($_POST["x"]))
-	{
-		echo "Sent:".$_POST["x"]."<br/>";
-		$myfile = fopen("message.txt", "w") or die("Unable to open file!");
-		fwrite($myfile, $_POST["x"]);
-	}
-	$myfile = fopen("http://192.168.7.112/message.txt", "r") or die("Unable to open file!");
-	echo "Message: ".fread($myfile,filesize("message.txt"));
-	fclose($myfile);
+if(isset($_POST['submit']))
+{
+if(isset($_POST["message"]))
+{
+$txt = "\n".$_POST["message"]."\n";
+$myfile = fopen("message.txt", "a+") or die("Unable to open file!");
+fwrite($myfile, $txt);
+fclose($myfile);
+}
+$fn = fopen("http://192.168.6.185/MyChatSystem/message.txt", "r") or die("Unable to open file!");
+while(! feof($fn))  {
+	$result = fgets($fn);
+	echo $result."<br/>";
+}
+fclose($fn);
+}
+
+if(isset($_POST['update']))
+{
+$fn = fopen("http://192.168.6.185/MyChatSystem/message.txt", "r") or die("Unable to open file!");
+while(! feof($fn))  {
+	$result = fgets($fn);
+	echo $result."<br/>";
+}
+fclose($fn);
+}
 ?>
+
+<HTML>
+	<TITLE>X8 Chat</TITLE>
+	<BODY>
+		<FORM ACTION="" METHOD="POST" name="sumbit">
+			<INPUT TYPE="TEXT" NAME="message">
+			<INPUT TYPE="submit" value="Send" name="submit">
+			<INPUT TYPE="submit" value="Update" name="update">
+		</FORM>
+	</BODY>
+</HTML>
